ec062919d17f019c47ad24c2ff7510f924e6ef52
Jeremiah <jeremiah@cloudspace.com>
2014-01-13 21:33:39 +0000

    Start working on the parse tree parser
    Not sure if this is the right solution.  Gives reasonable answers for
    simple code.  Obviously needs a full refactor.
    
    See MethodStatementLength for the parser reader and run it against
    test.rb to see if it works.

---
+++ lib/rubocop/cop/style/method_statement_length.rb
@@ -0,0 +1,74 @@
+# encoding: utf-8
+
+module Rubocop
+  module Cop
+    module Style
+      # This cop checks if the length a method exceeds some maximum value.
+      # Comment lines can optionally be ignored.
+      # The maximum allowed length is configurable.
+      class MethodLength < Cop
+        include CheckMethods
+        include CodeLength
+
+        private
+
+        def message
+          'Method statement has too many lines. [%d/%d]'
+        end
+
+        def code_length(node)
+          # total number of lines
+          # lines = node.loc.expression.source.lines.to_a[1..-2] || []
+          # lines.reject! { |line| irrelevant_line(line) }
+
+          # number of statements (naive solution with no nested code of any type)
+          lines = node.children[2].children
+
+          lines = rebuild_tree(node)
+
+          puts node.type
+          puts lines
+#          puts ("*" * 30)
+#          puts "Node: " + node.inspect
+#          puts "Children: " + node.children.inspect
+#          puts "Second Child: " + node.children[2].inspect
+#          puts "Grandchildren: " + node.children[2].children.inspect
+#          puts ("*" * 30)
+          # puts "Args: " + node.children[1].class.name
+          # puts node.children[1].children
+          # puts lines.size
+
+          lines.size
+        end
+
+        def rebuild_tree(node)
+          puts node.inspect
+
+          # this should never be reached, hopefully
+          if(!node)
+            return 0
+          # things that have the format (:name, (args), (begin ...))
+          elsif([:def, :defs].include?(node.type))
+            puts "def called"
+            return 1 + rebuild_tree(node.children[2])
+          # things that have the format (:name, (begin ...))
+          elsif([:begin, :kwbegin, :class, :sclass, :module].include?(node.type)) 
+            puts "begin/class/module called"
+            return 1 + node.children.map { |child| rebuild_tree(child) }.inject { |sum, child| sum + child }
+          # things that have the format(:if, (condition), (true code), (false code))
+          # handles if, elsif, and ternaries
+          elsif([:if].include?(node.type)) 
+            puts "if called"
+            lines = 1
+            lines += rebuild_tree(node.children[2])
+            lines += rebuild_tree(node.children[3]) if node.children.length == 4
+            return lines
+          else
+            puts "standard node called"
+            return 1
+          end
+        end
+      end
+    end
+  end
+end
---
+++ test.rb
@@ -0,0 +1,30 @@
+# teting
+module Foo
+  def bar
+    whee = 2
+
+    if(whee)
+      woo = 3
+      wooo = 4
+    end
+
+    {
+      a: whee,
+      b: woo,
+      c: wooo,
+      d: 1,
+      e: 1,
+      f: 1,
+      g: 1,
+      h: 1,
+      i: 1,
+      j: 1,
+      k: 1,
+      l: 1,
+      m: 1,
+      n: 1,
+      o: 1,
+      p: 1,
+    }
+  end
+end
